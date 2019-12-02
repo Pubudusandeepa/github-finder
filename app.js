8291fb5d06ad6a75056f90dafadd895c23fb9220
Pubudusandeepa <39061732+Pubudusandeepa@users.noreply.github.com>
2019-12-02 22:55:25 +0530

    Fetch display repos

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-//init github
-const github = new  Github;
 
-//search input  
+//init github
+//const github = new Github;
+const github = new Mic;
+
+//init ui
+const ui = new UI;
+//Search input
 const searchUser = document.getElementById('searchUser');
 
-//search input event listener
-searchUser.addEventListener('keyup', (e)=>{
+//search input event listner
+searchUser.addEventListener('keyup', (e) =>{
     //get input text
     const userText = e.target.value;
 
     if(userText !== ''){
-      //Make http call
-      github.getUser(userText).then(data =>{
-          console.log(data);
-      })
+        //Make http call
+        github.getUser(userText).then(data => {
+           if(data.profile.message === 'Not Found'){
+               //show alert
+               ui.showAlert('User not found', 'alert alert-danger');
+
+           }else{
+               //show profile
+               ui.showProfile(data.profile);
+               ui.showRepos(data.repos);
+           }
+        })
+    }else{
+        //clear profile
+        ui.clearProfile();
     }
-});
+})
--- github.js
+++ github.js
@@ -1,18 +1,27 @@
 
-class Github {
-    constructor(){
+class Mic {
+    constructor()
+    {
         this.client_id = 'f3496735b2654c2074c0';
-        this.client_secret = 'faa0745cb4d8160aeb191667c084dd096d213d22'
+        this.client_secret = 'faa0745cb4d8160aeb191667c084dd096d213d22';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?
         client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}
+        &sort=${this.repos_sort}
+        client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
 
         return{
-             profile
+             profile,
+             repos
         }
     }
 }
